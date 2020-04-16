096c41c46dabc2ef1eeccfea69f73f091d5f59c0
Colin Brayton <gringolalia@gmail.com>
2020-04-16 16:42:06 -0300

    deploy

---
+++ precache-manifest.bf8fc650ad0a1c233ab5e49f5c00b92e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6872ece7dd69fabaa237",
+    "url": "/ibuprofen-then-again/css/app.90198406.css"
+  },
+  {
+    "revision": "f9553e650231f7169d7a",
+    "url": "/ibuprofen-then-again/css/chunk-vendors.6bbededb.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/ibuprofen-then-again/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bccf4b57e6a91ba64befb5bf08b0de43",
+    "url": "/ibuprofen-then-again/index.html"
+  },
+  {
+    "revision": "ee5918811251a6afb6bb",
+    "url": "/ibuprofen-then-again/js/about.97aa91af.js"
+  },
+  {
+    "revision": "6872ece7dd69fabaa237",
+    "url": "/ibuprofen-then-again/js/app.3d972fed.js"
+  },
+  {
+    "revision": "f9553e650231f7169d7a",
+    "url": "/ibuprofen-then-again/js/chunk-vendors.c8a25988.js"
+  },
+  {
+    "revision": "207d58af139df02de4236873438d1d12",
+    "url": "/ibuprofen-then-again/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ibuprofen-then-again/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ibuprofen-then-again/precache-manifest.bf8fc650ad0a1c233ab5e49f5c00b92e.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "ibuprofen-then-again"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
